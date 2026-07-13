05499ff3e56182772c67d156622e73f818c81895
King Sam Animations <224757196+kingsamanimations@users.noreply.github.com>
2026-07-13 12:58:46 +0100

    Add animation and pop-up in script.js and style.css
    q

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function toggleModal() {
+  var popup = document.getElementById("popup-modal");
+  const backdrop = document.querySelector('.backdrop')
+  popup.classList.toggle("show");
+  backdrop.classList.toggle('show');
+}
+
